daf21d6902de16027eeb0a6236e324b53cfb0658
Graham Forrester <graham_forrester1@hotmail.com>
2018-04-23 21:35:29 +0100

    Added Index Controller

---
+++ controllers/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = new express.Router();
+
+router.use('/films', require('./films_controller.js'));
+router.use('/books', require('./books_controller.js'));
+
+module.exports = router;
